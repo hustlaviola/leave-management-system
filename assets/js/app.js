ff2e28e8e18d739cab5ea1d9cd9fd553606b6c0f
Viola <hustlaviola@gmail.com>
2019-05-30 14:09:59 +0100

    Create a function to get all users create a hidden div to save all users

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,17 @@
 const alert = document.querySelector(".alert");
-  
+const users = document.querySelector(".users");
+
+let xhr = new XMLHttpRequest();
+
+const url = `http://localhost:3000/users`;
+
+function getUsers() {
+  xhr.open('GET', url, true);
+
+  xhr.onload = function() {
+    if(this.status === 200) {
+      users.innerHTML = this.responseText;
+    }
+  }
+  xhr.send()
+}
