35f4f025c5861cfe18a20e9620db43222c352b7e
Viola <hustlaviola@gmail.com>
2019-05-31 09:09:17 +0100

    Add admin approve request functionality

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,35 +1,60 @@
 const requests = document.querySelector(".reqs");
 
 const xhr = new XMLHttpRequest();
+const users = [];
 
 const url = `http://localhost:3000/requests`;
 
 function getRequests() {
-    xhr.open("GET", url, true);
-  
-    xhr.onload = function() {
-      if (this.status === 200) {
-        const response = JSON.parse(this.responseText);
-        let output = "";
+  xhr.open("GET", url, true);
 
-        response.forEach( data => {
-            output += `
+  xhr.onload = function() {
+    if (this.status === 200) {
+      const response = JSON.parse(this.responseText);
+      let output = "";
+
+      response.forEach(data => {
+        users.push(data);
+        output += `
             <br>
             <div><span class="current">Type: </span>${data["type"]}</div>
-            <div><span class="current">Description: </span>${data["description"]}</div>
-             <div class="two-btn mt-2">
-            <button onclick="" class="btn-small btn-primary mb-1">
+            <div><span class="current">Description: </span>${
+              data["description"]
+            }</div>
+            <div><span class="current">Status: </span>${data["status"]}</div>
+             <div  id="${ data["id"]}"class="two-btn mt-2">
+            <button onclick="approve(event)" class="btn-small btn-primary mb-1">
             Approve
         </button>
-        <button onclick="" class="btn-small btn-danger mb-1">
+        <button onclick="decline(event)" class="btn-small btn-danger mb-1">
           Decline
         </button>
       </div>
             <br>
-            <hr>`
-        });
-        requests.innerHTML = output
-      }
-    };
-    xhr.send();
-  }
+            <hr>`;
+      });
+      requests.innerHTML = output;
+    }
+  };
+  xhr.send();
+}
+
+const approve = event => {
+  const target = event.target;
+  const id = target.parentNode.id;
+  const url = `http://localhost:3000/requests/${id}`;
+
+
+  xhr.open("PATCH", url, true);
+
+  xhr.setRequestHeader("content-Type", "application/json");
+
+  xhr.onload = function() {
+    if (this.status === 200) {
+    }
+  };
+  const data = JSON.stringify({
+    status: "approved"
+  });
+  xhr.send(data);
+};
